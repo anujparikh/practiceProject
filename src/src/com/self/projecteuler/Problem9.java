920ccf3e8b1670496906f55dfbcb97710cbd9e1a
anujparikh <anujparikh1@gmail.com>
2016-09-11 20:02:36 -0400

    modify problem 9

--- src/src/com/self/projecteuler/Problem9.java
+++ src/src/com/self/projecteuler/Problem9.java
@@ -2,11 +2,12 @@ package src.com.self.projecteuler;
 
 public class Problem9 {
 
-    public Integer pythagoreanTriplet() {
-        for (int i = 500; i > 0; i--) {
-            for (int j = 500; j > 0; j--) {
+    public Integer pythagoreanTriplet(int sum) {
+        int startValue = sum/2;
+        for (int i = startValue; i > 0; i--) {
+            for (int j = startValue; j > 0; j--) {
                 int sqrt = (int) Math.sqrt((i * i) + (j * j));
-                if (sqrt * sqrt == ((i * i) + (j * j)) && i + j + sqrt == 1000) {
+                if (sqrt * sqrt == ((i * i) + (j * j)) && i + j + sqrt == sum) {
                     System.out.println("a: " + i + "\nb: " + j + "\nc: " + sqrt);
                     return i * j * sqrt;
                 }
--- test/com/self/projecteuler/Problem9Test.java
+++ test/com/self/projecteuler/Problem9Test.java
@@ -9,6 +9,6 @@ public class Problem9Test {
     @Test
     public void pythagoreanTripletTest() {
         Problem9 testObject = new Problem9();
-        Assert.assertEquals((long) 31875000, (long) testObject.pythagoreanTriplet());
+        Assert.assertEquals((long) 31875000, (long) testObject.pythagoreanTriplet(1000));
     }
 }
