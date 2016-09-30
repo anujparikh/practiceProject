bdb67c611b98a7a44d5a23ddaab7775c4f9a278d
anujparikh <anujparikh1@gmail.com>
2016-09-30 14:37:12 -0400

    rotate matrix

--- src/src/com/self/book/Anagrams.java
+++ src/src/com/self/book/Anagrams.java
@@ -4,4 +4,28 @@ package src.com.self.book;
  * Created by anujparikh on 9/30/16.
  */
 public class Anagrams {
+
+    public static boolean areAragrams(String s1, String s2) {
+
+        s1 = s1.toLowerCase().replace(" ", "");
+        s2 = s2.toLowerCase().replace(" ", "");
+
+        if (s1.length() != s2.length()) return false;
+
+        StringBuilder sb1 = new StringBuilder(s1);
+        StringBuilder sb2 = new StringBuilder(s2);
+
+        for (int i = 0; i < sb1.length(); i++) {
+            if (sb2.indexOf(String.valueOf(sb1.charAt(i))) == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(areAragrams("abcd", "dcab"));
+        System.out.println(areAragrams("abcd", "abcdcd"));
+        System.out.println(areAragrams("Clint Eastwood!", "Old! West action"));
+    }
 }
